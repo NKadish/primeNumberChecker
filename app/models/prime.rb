7947b9e7c31ed0678c5c087f3f704097ed9eb009
NKadish <noahkadish@hotmail.com>
2021-03-22 20:31:44 +0000

    Account ing for edge cases

--- app/models/prime.rb
+++ app/models/prime.rb
@@ -2,7 +2,12 @@ class Prime
 
   def self.prime?(a)
     num = a.to_i
-    (2..(num - 1)).each do |n|
+    # Wasn't catching 0 or 1, was saying they were prime, added a catch
+    if num == 0 || 1
+      return false
+    end
+    
+    (2..(Math.sqrt(num))).each do |n|
       return false if num % n == 0
     end
     true
