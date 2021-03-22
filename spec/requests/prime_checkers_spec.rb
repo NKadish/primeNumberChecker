80d3a0da5cc2eafead34cc28bc33241af1e99558
NKadish <noahkadish@hotmail.com>
2021-03-22 22:25:54 +0000

    Capybara install

--- spec/requests/prime_checkers_spec.rb
+++ spec/requests/prime_checkers_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "PrimeCheckers", type: :request do
   describe "GET /" do
-    it "works!" do
+    it "Loads the app" do
       get '/'
       expect(response).to have_http_status(200)
     end
+
+    it "Successfully calculates and shows if the input is prime" do
+      get '/'
+      fill_in "number", with: "2"
+      
+    end
+
   end
 end
