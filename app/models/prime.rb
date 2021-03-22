e4ac34c5a7ca56fec5bdd5fe887f16a6189ad4eb
NKadish <noahkadish@hotmail.com>
2021-03-22 22:07:04 +0000

    Bugfix prime 0 or 1 catcher

--- app/models/prime.rb
+++ app/models/prime.rb
@@ -4,9 +4,9 @@ class Prime
     # Turns the input from a string into an integer
     num = a.to_i
     # Wasn't catching 0 or 1, was saying they were prime, added a catch
-    # if num == 0 || 1
-    #   return false
-    # end
+    if num == 0 || num == 1
+      return false
+    end
     # Checks each number up to their sqrt, looks to see if there is no remainder after num % n, if there is it is true for that n 
     (2..(Math.sqrt(num))).each do |n|
       return false if num % n == 0
