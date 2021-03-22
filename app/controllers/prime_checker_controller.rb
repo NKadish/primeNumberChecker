d1cd71664d91493aa3b0201b1536c2d78b488bbe
NKadish <noahkadish@hotmail.com>
2021-03-22 23:11:14 +0000

    Trying to implement a cache

--- app/controllers/prime_checker_controller.rb
+++ app/controllers/prime_checker_controller.rb
@@ -1,12 +1,19 @@
 class PrimeCheckerController < ApplicationController
   protect_from_forgery with: :exception
+  cache = ActiveSupport::Cache::MemoryStore.new
+
 
   def show
   end
 
   def new
-    @result = Prime.send(:prime?, *[params[:a]])
     @number = params[:a].to_i
+    if cache.read(params[:a])
+      @result = cache.read(params[:a])
+    else 
+      @result = Prime.send(:prime?, *[params[:a]])
+      cache.write(params[:a], @result)
+    end
     render :show
   end
   
