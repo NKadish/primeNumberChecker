eee1f59eea308cc3d66c2cae49fff8a69c536709
NKadish <noahkadish@hotmail.com>
2021-03-22 21:48:15 +0000

    Rspec setup

--- test/controllers/prime_checker_controller_test.rb
+++ test/controllers/prime_checker_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PrimeCheckerControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get prime_checker_show_url
+    get '/prime_checker'
     assert_response :success
   end
 
