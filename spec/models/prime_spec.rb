5d6ef2c989e62464300f495160fd1f7bf6560fb8
NKadish <noahkadish@hotmail.com>
2021-03-22 22:09:28 +0000

    Add more prime testing

--- spec/models/prime_spec.rb
+++ spec/models/prime_spec.rb
@@ -6,5 +6,15 @@ RSpec.describe Prime, type: :model do
       @result = Prime.send(:prime?, '2')
       expect(@result).to be true
     end
+
+    it 'Should return false for a number that is not prime' do
+      @result = Prime.send(:prime?, '4')
+      expect(@result).to be false
+    end
+
+    it 'Should return false for a string that is not a number' do
+      @result = Prime.send(:prime?, 'Hello world')
+      expect(@result).to be false
+    end
   end
 end
