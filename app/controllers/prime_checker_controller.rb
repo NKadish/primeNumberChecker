47db9652d2f3af4355d2e91ff7d605e936aa8974
NKadish <noahkadish@hotmail.com>
2021-03-23 00:30:08 +0000

    Got caching to persist

--- app/controllers/prime_checker_controller.rb
+++ app/controllers/prime_checker_controller.rb
@@ -7,15 +7,13 @@ class PrimeCheckerController < ApplicationController
   def new
     # @cache = ActiveSupport::Cache::MemoryStore.new
     @number = params[:a].to_i
-    if Rails.cache.read(params[:a]) == 'true' || Rails.cache.read(params[:a]) == 'false'
-      puts "hello world"
+    cached = Rails.cache.read(params[:a])
+    if cached == true || cached == false
       @result = Rails.cache.read(params[:a])
     else 
       @result = Prime.send(:prime?, params[:a])
       Rails.cache.write(params[:a], @result)
-      Rails.cache.write("4", false)
       puts Rails.cache.read(params[:a])
-      puts Rails.cache.read("4")
     end
     render :show
   end
